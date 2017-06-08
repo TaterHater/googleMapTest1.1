48bd4a5794e59bb1a5146fd2b89e2f980b4744fd
Nick B <nick_beeson@yahoo.com>
2017-06-08 11:19:57 -0600

    changed repo directory

---
+++ googleMapTest1/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace googleMapTest1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public ActionResult Index(float lat, float log)
+        {
+            ViewBag.lat = 0;
+            ViewBag.log = 0;
+            ViewBag.lat = lat;
+            ViewBag.log = log;
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(string name, int ID = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = ID;
+            return View();
+        }
+    }
+}
